fde33a348c4727847959a89ce0fa44fea199134a
bitshadowdev <jaimecrispit@gmail.com>
2025-10-25 11:09:41 -0300

    Cascara

--- src/agent.py
+++ src/agent.py
@@ -1,15 +1,22 @@
 """Es la clase base del agente"""
 from abc import ABC, abstractmethod
+from event import Event
 
-class Agent(ABC):
+class AbstractAgent(ABC):
     """Es es un contrato general que tienen que 
     seguir todos los agentes"""
     @abstractmethod
     def update_system_promt(self):
         pass
 
-
-
-class Agent:
-    def __init__(self):
+    # Get response 
+    @abstractmethod
+    def get_response(self) -> Event:
         pass
+
+    
+
+class BaseAgent(AbstractAgent):
+    def __init__(self):
+        self.nombre = "Default Agent"
+        
---
+++ src/event.py
@@ -0,0 +1,14 @@
+from event_manager import EventManager
+import datetime 
+
+class Event:
+    def __init__(self, event_type, event_name, event_description):
+        self.event_time = datetime.datetime.now()
+        # Registro de eventos
+        self.event_manager_ref = EventManager()
+        
+        # tipo de evento, crear herramienta, crear agente, refinar prompt etc.
+        self.event_type = event_type
+        self.event_name = event_name 
+        # Esta descripcion proviene de la IA
+        self.event_description = event_description
